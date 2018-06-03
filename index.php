beebaa6b4de577c1e80c1baeb0fed9e8b8ae9229
ilessing <ilessing@library.ucsb.edu>
2018-06-02 17:53:28 -0700

    testing query strings

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 echo "Hello World<br><br>";
 
-echo "HTTP_HOST: ". $_SERVER['HTTP_HOST'] ."<br>";
+echo "HTTP_HOST: ". $_SERVER['HTTP_HOST'] ."<br><br>";
+
+if($_GET['foo'] == 'bar'){
+	echo "you seem to have a query string for foo equal to bar<br><br>";
+}else{
+	"no foo bar<br><br>";
+}
